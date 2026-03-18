643379652d83735915cce77ffec842ccafceea55
Pramod Kumar <pramodkumar1306@gmail.com>
2026-03-18 14:17:25 +0530

    simple app

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("🚀 Hello from AKS Pipeline App!");
+});
+
+const PORT = 4000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
